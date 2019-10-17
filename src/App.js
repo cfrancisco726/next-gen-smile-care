fe23f2921555bbda0d002f54f574e9aa61899b9e
cfrancisco726 <cfrancisco726@gmail.com>
2019-10-17 00:51:24 -0400

    media querie added for nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, {Component } from 'react';
 import Toolbar from './components/Toolbar/Toolbar'
 import SideDrawer from './components/SideDrawer/SideDrawer'
+import Backdrop from './components/Backdrop/Backdrop'
 
-function App() {
-  return (
-    <div style={{height: '100%'}}>
-      <Toolbar />
-      <SideDrawer/>
-      <main style={{marginTop: '64px'}}>
-        <p>This is the page content</p>
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  }
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen}
+    })
+  }
+
+  backdropClickHandler = () => {
+    this.setState({sideDrawerOpen: false})
+  }
+  render(){
+    let backdrop;
+
+    if(this.state.sideDrawerOpen){
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
+    return (
+      <div style={{height: '100%'}}>
+        <Toolbar  drawerClickHandler={this.drawerToggleClickHandler}/>  
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
+        <main style={{marginTop: '64px'}}>
+        </main>
+      </div>
+   );
+  }
 }
 
 export default App;
