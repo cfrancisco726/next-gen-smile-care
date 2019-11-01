daa2826785e504da87817dafd9f7441477ae1f37
cfrancisco726 <cfrancisco726@gmail.com>
2019-10-31 23:53:00 -0400

    form works for emailjs

--- src/components/Pages/Appointment/Appointment.js
+++ src/components/Pages/Appointment/Appointment.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default class extends React.Component {
+import "./Appointment.css";
+
+class Appointment extends Component {
   constructor(props) {
     super(props);
     this.state = { name: "", email: "", phone: "", message: "" };
+
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+
+    this.setState({ [event.target.name]: value });
+    console.log("handle");
+  }
+
+  handleSubmit(event) {
+    const templateId = "template_qWttzqDG";
+
+    this.sendFeedback(templateId, {
+      message_html: this.state.message,
+      from_name: this.state.name,
+      reply_to: this.state.email,
+      phone_num: this.state.phone
+    });
+    console.log("submit");
+  }
+
+  sendFeedback(templateId, variables) {
+    window.emailjs
+      .send("nextgensmilecare", templateId, variables)
+      .then(res => {
+        console.log("Email successfully sent!");
+      })
+      .catch(err => console.error("failed:", err));
   }
 
   render() {
@@ -17,6 +48,7 @@ export default class extends React.Component {
           <input
             type="text"
             id="name"
+            name="name"
             required
             value={this.state.name}
             onChange={this.handleChange}
@@ -27,55 +59,42 @@ export default class extends React.Component {
           <input
             type="email"
             id="email"
+            name="email"
             required
             value={this.state.email}
             onChange={this.handleChange}
           />
         </label>
         <label>
+          phone
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={this.state.phone}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Message
           <textarea
-            id="test-mailing"
-            name="test-mailing"
+            id="message"
+            name="message"
+            value={this.state.message}
             onChange={this.handleChange}
             placeholder="Post some lorem ipsum here"
             required
-            value={this.state.message}
-            style={{ width: "100%", height: "150px" }}
           />
         </label>
         <input
           type="button"
-          value="Submit"
-          className="btn btn--submit"
+          value="Request Appointment"
+          className="btn btn-submit"
           onClick={this.handleSubmit}
         />
       </form>
     );
   }
-
-  handleChange(event) {
-    const { value } = event.target;
-
-    this.setState({ [event.target.name]: value });
-  }
-
-  handleSubmit(event) {
-    const templateId = "template_qWttzqDG";
-
-    this.sendFeedback(templateId, {
-      message_html: this.state.feedback,
-      from_name: this.state.name,
-      reply_to: this.state.email
-    });
-  }
-
-  sendFeedback(templateId, variables) {
-    window.emailjs
-      .send("nextgensmilecare", templateId, variables)
-      .then(res => {
-        console.log("Email successfully sent!");
-      })
-      // Handle errors here however you like, or use a React error boundary
-      .catch(err => console.error("failed:", err));
-  }
 }
+
+export default Appointment;
