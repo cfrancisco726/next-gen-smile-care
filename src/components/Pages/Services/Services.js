ffdc3281f25b0fe8b31cc69b8819b1cc0c168d5a
cfrancisco726 <cfrancisco726@gmail.com>
2020-01-10 17:44:00 -0500

    add services

--- src/components/Pages/Services/Services.js
+++ src/components/Pages/Services/Services.js
@@ -1,10 +1,74 @@
 import React from "react";
 import "./Services.css";
+import { Link } from "react-router-dom";
 
 function Services() {
   return (
     <div className="services-pg">
-      <h1>Services</h1>
+      <div>
+        <img src={require("../../../images/our_office.jpg")} alt="our_office" />
+      </div>
+      <div>
+        <h1>OUR SERVICES</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s. Lorem Ipsum has been the industry's standard dummy
+          text ever since the 1500s.
+        </p>
+      </div>
+      <div className="services-row">
+        <div className="services-item">
+          <img src={require("../../../images/cleaning.svg")} alt="cleaning" />
+          <div className="services-text">
+            <h4>Cleaning and Whitening</h4>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+        </div>
+        <div className="services-item">
+          <img
+            src={require("../../../images/invisalign.svg")}
+            alt="invisalign"
+          />
+          <div className="services-text">
+            <h4>Invisalign</h4>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+        </div>
+        <div className="services-item">
+          <img src={require("../../../images/implant.svg")} alt="impant" />
+          <div className="services-text">
+            <h4>Dental Implant</h4>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+        </div>
+        <div className="services-item">
+          <img src={require("../../../images/perio.svg")} alt="perio" />
+          <div className="services-text">
+            <h4>Periodontal Treatment</h4>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s
+            </p>
+          </div>
+        </div>
+      </div>
+      <Link to="/services">
+        <button type="button">View All Services</button>
+      </Link>
     </div>
   );
 }
